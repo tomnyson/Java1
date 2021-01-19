a95574057d2d97a07e096b6721867b21ed404a4b
tomnyson <tabletkindfire@gmail.com>
2021-01-19 17:23:31 +0700

    update code

--- src/ud1/Ud1.java
+++ src/ud1/Ud1.java
@@ -9,6 +9,9 @@ import java.util.Properties;
 import java.util.Scanner;
 
 /**
+ * PK01991	Phan Thanh Thắng PK01996	Phạm Xuân Hướng PK02125	Lục Thị Quỳnh Nga
+ * PK02011	Lê Đình Lực /** PK01989	Đặng Thế Nguyên PK01876	Hoàng Văn Thành
+ * PT163LT	PK02011	Lê Đình Lực chép 20 lần cách khai báo các kiểu dữ
  *
  * @author tomnyson
  */
@@ -34,7 +37,7 @@ public class Ud1 {
         1. nhập vào 1 số từ 10 - 100;
         2. nếu không phải thì bắt nhập 
         3. xuất ra các s nguyên tố từ n - 100;
-        */
+         */
 //        int n;
         Scanner scaner = new Scanner(System.in);
 //        System.out.println("Nhập giá trị n: ");
@@ -47,14 +50,39 @@ public class Ud1 {
 //            }
 //        }
 //        int[] a = {0, 1, 2, 3, 4, 5,6};
-          double[] b = new double[5];
-          System.out.println("length"+b.length);
-         for(int i=0; i< b.length; i++) {
-             System.out.println("Nhập giá trị của mảng a vi trí: "+ i);
-              b[i] = scaner.nextDouble();
+        int[] b = new int[5];
+        System.out.println("length" + b.length);
+        for (int i = 0; i < b.length; i++) {
+            System.out.println("Nhập giá trị của mảng a vi trí: " + i);
+            b[i] = scaner.nextInt();
         }
-        for(int i=0; i< b.length; i++) {
-            System.out.println(b[i]);
+        System.out.println("xuat ra danh sach chan");
+        int dem = 0;
+        int sum = 0;
+        int max = b[0];
+        for (int i = 0; i < b.length; i++) {
+            if (b[i] % 2 == 0) {
+                System.out.printf("vị trí  a[%d] giá trị %d", i, b[i]);
+                dem++;
+            }
+            sum += b[i];
+            if (max < b[i]) {
+                max = b[i];
+            }
+        } 
+        int nhap,bienDem=0;
+       
+        System.out.println("Ban hay nhap so can tim:");
+        nhap = scaner.nextInt();
+        for(int i = 0; i < b.length; i++){
+            if (nhap==b[i]){
+               System.out.println("So ban tim co trong ["+i+"]"+b[i]); 
+            bienDem++;
+            }
         }
+        if(bienDem==0){ 
+        System.out.println("So ban tim khong co trong  ");}
+        System.out.println("số lượng phần tử chbẳn của mảng là: " + dem);
+        System.out.println("gía trị lớn nhất: " + max);
     }
 }
